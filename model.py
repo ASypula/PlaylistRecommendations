c43914864efa6bf97d9d60c1e08c78ac2296a4e1
Byku <sztanga.eryk@gmail.com>
2023-05-09 13:23:32 +0200

    Made reccomendation into class

--- model.py
+++ model.py
@@ -1,96 +1,98 @@
-from jsonLoader import *
+from jsonLoader import getArtists, getTracks
 import pandas as pd
 from sklearn.metrics.pairwise import cosine_similarity
 from sklearn.feature_extraction.text import CountVectorizer
 import re
 
-tracks = getTracks()
-artists = getArtists()
 
-# Superstar
-# Wannabe
-# You and Me
+class Reccomender:
+    tracks =[]
+    artists = []
+    mapping = []
+    tracks2 = []
 
-tracks = pd.DataFrame.from_dict(tracks)
-artists = pd.DataFrame.from_dict(artists)
+    def loadData(self):
+        self.tracks = getTracks()
+        self.artists = getArtists()
+        self.tracks = pd.DataFrame.from_dict(self.tracks)
+        self.artists = pd.DataFrame.from_dict(self.artists)
+        self.mapping = pd.Series(self.tracks.index,index = self.tracks['name'])
 
-def recommend_most_popular(col,col_value,top_n=5):
-    return tracks[tracks[col]>=col_value].sort_values(by='popularity',ascending = False).head(top_n)[['name',col,'popularity']]
-
-# print(recommend_most_popular('valence', 0.9))
-
-mapping = pd.Series(tracks.index,index = tracks['name'])
-# print(tracks)
-
-tracks2 = tracks.copy()
-tracks2.drop(labels=("id"), axis=1, inplace=True)
-tracks2.drop(labels=("name"), axis=1, inplace=True)
-
-def dropRedundant():
-    tracks2.drop(labels=("loudness"), axis=1, inplace=True)
-    tracks2.drop(labels=("acousticness"), axis=1, inplace=True)
-    tracks2.drop(labels=("valence"), axis=1, inplace=True)
-
-def applyDateParser(dateString):
-    regs = r"[0-9]{4}-[0-9\-]*"
-    if (re.match(regs, dateString)):
-        return int(dateString[:4])
-    else:
-        return int(dateString)
+    def initVector(self):
+        self.tracks2 = self.tracks.copy()
+        self.tracks2.drop(labels=("id"), axis=1, inplace=True)
+        self.tracks2.drop(labels=("name"), axis=1, inplace=True)
 
 
-def parseDates():
-    tracks2['release_date'] = tracks2['release_date'].apply(applyDateParser)
-
-def dropDates():
-    tracks2.drop(labels=("release_date"), axis=1, inplace=True)
+    def recommend_most_popular(self,col,col_value,top_n=5):
+        return self.tracks[self.tracks[col]>=col_value].sort_values(by='popularity',ascending = False).head(top_n)[['name',col,'popularity']]
 
 
-def dropArtists():
-    tracks2.drop(labels=("id_artist"), axis=1, inplace=True)
-    
+    def dropRedundant(self):
+        self.tracks2.drop(labels=("loudness"), axis=1, inplace=True)
+        self.tracks2.drop(labels=("acousticness"), axis=1, inplace=True)
+        self.tracks2.drop(labels=("valence"), axis=1, inplace=True)
 
-def applyGenres(id_artist):
-    # df.loc[df['column_name'] == some_value]
-    genres = artists.loc[artists['id'] == id_artist]
-    genres = genres.iloc[0]['genres']
-    genres = ",".join(genres).lower()
-    return genres
-
-def countEncodeGenres(tracks2):
-    tracks2.rename(columns= {"id_artist":"genres"}, inplace=True)
-    tracks2["genres"] = tracks2["genres"].apply(applyGenres)
-    vectorizer = CountVectorizer(tokenizer=lambda x: x.split(','))
-    X = vectorizer.fit_transform(tracks2['genres'])
-    X.toarray()
-    genrestags = vectorizer.get_feature_names_out()
-    countVectorGenres = pd.DataFrame(X.toarray(),columns=genrestags)
-    # print(countVectorGenres.head())
-
-    tracks2.drop(labels=("genres"), axis=1, inplace=True)
-    tracks2 = tracks2.join(countVectorGenres)
-
-def oneHotArtists(tracks2):
-    one_hot_artist = pd.get_dummies(tracks2.id_artist, prefix="id_a", dtype=float)
-    tracks2.drop(labels=("id_artist"), axis=1, inplace=True)
-    tracks2 = tracks2.join(one_hot_artist)
+    def applyDateParser(self,dateString):
+        regs = r"[0-9]{4}-[0-9\-]*"
+        if (re.match(regs, dateString)):
+            return int(dateString[:4])
+        else:
+            return int(dateString)
 
 
+    def parseDates(self):
+        self.tracks2['release_date'] = self.tracks2['release_date'].apply(self.applyDateParser)
+
+    def dropDates(self):
+        self.tracks2.drop(labels=("release_date"), axis=1, inplace=True)
 
 
+    def dropArtists(self):
+        self.tracks2.drop(labels=("id_artist"), axis=1, inplace=True)
+        
 
-dropRedundant()
-parseDates()
-countEncodeGenres(tracks2)
-# dropArtists()
-similarityMatrix = cosine_similarity(tracks2, tracks2)
+    def applyGenres(self,id_artist):
+        # df.loc[df['column_name'] == some_value]
+        genres = self.artists.loc[self.artists['id'] == id_artist]
+        genres = genres.iloc[0]['genres']
+        genres = ",".join(genres).lower()
+        return genres
 
-def reccomend_similar(songName):
-    songIndex = mapping[songName]
-    similarityScore = list(enumerate(similarityMatrix[songIndex]))
-    similarityScore = sorted(similarityScore, key=lambda x: x[1], reverse=True)
-    similarityScore = similarityScore[1:15]
-    indices = [i[0] for i in similarityScore]
-    return (tracks['name'].iloc[indices])
+    def countEncodeGenres(self):
+        self.tracks2.rename(columns= {"id_artist":"genres"}, inplace=True)
+        self.tracks2["genres"] = self.tracks2["genres"].apply(self.applyGenres)
+        vectorizer = CountVectorizer(tokenizer=lambda x: x.split(','), token_pattern=None)
+        X = vectorizer.fit_transform(self.tracks2['genres'])
+        X.toarray()
+        genrestags = vectorizer.get_feature_names_out()
+        countVectorGenres = pd.DataFrame(X.toarray(),columns=genrestags)
+        # print(countVectorGenres.head())
 
-print(reccomend_similar('Wannabe'))
+        self.tracks2.drop(labels=("genres"), axis=1, inplace=True)
+        self.tracks2 = self.tracks2.join(countVectorGenres)
+
+    def oneHotArtists(self):
+        one_hot_artist = pd.get_dummies(self.tracks2.id_artist, prefix="id_a", dtype=float)
+        self.tracks2.drop(labels=("id_artist"), axis=1, inplace=True)
+        self.tracks2 = self.tracks2.join(one_hot_artist)
+
+
+    def reccomend_similar(self,songName):
+        songIndex = self.mapping[songName]
+        similarityMatrix = cosine_similarity(self.tracks2, self.tracks2)
+        similarityScore = list(enumerate(similarityMatrix[songIndex]))
+        similarityScore = sorted(similarityScore, key=lambda x: x[1], reverse=True)
+        similarityScore = similarityScore[1:15]
+        indices = [i[0] for i in similarityScore]
+        return (self.tracks['name'].iloc[indices])
+
+r = Reccomender()
+r.loadData()
+r.initVector()
+r.dropRedundant()
+r.parseDates()
+r.countEncodeGenres()
+# r.dropArtists()
+
+print(r.reccomend_similar('Wannabe'))
